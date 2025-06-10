d202b0f8472e5dd0233db96ca05c57cf4bb8ac4a
Pablo Lires <pablo@librecomunicacion.net>
2025-06-10 15:53:22 -0300

    cambios en eventos del home

--- src/components/home/Events.jsx
+++ src/components/home/Events.jsx
@@ -1,6 +1,8 @@
-import logoAfa from '@/assets/img/afa-white.webp'
-import escudos from '@/assets/img/escudos.webp'
+import { Link } from 'react-router-dom'
+import tienda from '@/assets/img/tienda-albiceleste.webp'
+import casa from '@/assets/img/casa-del-campeon.webp'
 import bresh from '@/assets/img/bresh.webp'
+import logoCopa from '@/assets/img/logos-copa.webp'
 
 import './events.css'
 
@@ -12,79 +14,87 @@ const Events = () => {
           <h2>PRÓXIMOS EVENTOS:</h2>
         </div>
       </div>
-      <div className='row'>
-        <div className='col-md-6 col-xxl-3'>
-          <div data-aos='fade-up' className='evento bresh'>
-            <div className='left'>
-              <img src={bresh} alt='bresh' />
-            </div>
-            <div className='separator'></div>
-            <div className='right'>
-              <div className='content'>
-                <span>MUY PRONTO</span>
-                <button className='btn comprar transition'>
-                  COMPRAR TICKETS
-                </button>
-              </div>
+
+      <div className='row contentEventos'>
+        {/* Tienda */}
+        <Link to='#' data-aos='fade-up' className='evento tienda'>
+          <div className='left'>
+            <img src={tienda} alt='tienda' />
+          </div>
+          <div className='separator'></div>
+          <div className='right'>
+            <div className='content'>
+              <span>MUY PRONTO</span>
+              <button className='btn comprar transition'>VER TIENDA</button>
             </div>
           </div>
-        </div>
-        <div className='col-md-6 col-xxl-3'>
-          <div data-aos='fade-up' className='evento experiencia'>
-            <div className='left'>
-              <img src={logoAfa} alt='logo afa' />
-              <div className='content'>
-                <span>EXPERIENCIA</span>
-                <button className='btn comprar transition'>
-                  COMPRAR TICKETS
-                </button>
-              </div>
-            </div>
-            <div className='separator'></div>
-            <div className='right'>
-              <span>PROXIMAMENTE</span>
+        </Link>
+        {/* Tienda end */}
+
+        {/* Experiencia */}
+        <Link to='#' data-aos='fade-up' className='evento experiencia'>
+          <div className='left'>
+            <span>EXPERIENCIA</span>
+          </div>
+          <div className='separator'></div>
+          <div className='right'>
+            <img src={casa} alt='casa' />
+          </div>
+        </Link>
+        {/* Experiencia end */}
+
+        {/* Bresh */}
+        <Link to='#' data-aos='fade-up' className='evento bresh'>
+          <div className='left'>
+            <img src={bresh} alt='bresh' />
+          </div>
+          <div className='separator'></div>
+          <div className='right'>
+            <div className='content'>
+              <span>14 de Junio - MIAMI</span>
+              <button className='btn comprar transition'>
+                COMPRAR TICKETS
+              </button>
             </div>
           </div>
-        </div>
-        <div className='col-md-6 col-xxl-3'>
-          <div data-aos='fade-up' className='evento fanzone'>
-            <div className='left'>
-              <span>FANZONE</span>
-            </div>
-            <div className='separator'></div>
-            <div className='right'>
-              <div className='content'>
-                <span>PROXIMAMENTE</span>
-                <button className='btn comprar transition'>
-                  COMPRAR TICKETS
-                </button>
-              </div>
+        </Link>
+        {/* Bresh end */}
+
+        {/* Fixture */}
+        <Link to='#' data-aos='fade-up' className='evento fixture'>
+          <div className='left'>
+            <img
+              className='img-fluid escudos'
+              src={logoCopa}
+              alt='logo afa + copa'
+            />
+          </div>
+          <div className='separator'></div>
+          <div className='right'>
+            <div className='content'>
+              <span>FIXTURE</span>
+              <button className='btn comprar transition'>VER FIXTURE</button>
             </div>
           </div>
-        </div>
-        <div className='col-md-6 col-xxl-3'>
-          <div data-aos='fade-up' className='evento amistoso'>
-            <div className='left'>
-              <div className='content'>
-                <span>PARTIDO AMISTOSO</span>
-                <img
-                  className='img-fluid escudos'
-                  src={escudos}
-                  alt='escudos partido amistoso'
-                />
-              </div>
-            </div>
-            <div className='separator'></div>
-            <div className='right'>
-              <div className='content'>
-                <span>PROXIMAMENTE</span>
-                <button className='btn comprar transition'>
-                  COMPRAR TICKETS
-                </button>
-              </div>
+        </Link>
+        {/* Fixture end */}
+
+        {/* FanZone */}
+        <Link to='#' data-aos='fade-up' className='evento fanzone'>
+          <div className='left'>
+            <span>FANZONE</span>
+          </div>
+          <div className='separator'></div>
+          <div className='right'>
+            <div className='content'>
+              <span>MIAMI 2026</span>
+              <button className='btn comprar transition'>
+                COMPRAR TICKETS
+              </button>
             </div>
           </div>
-        </div>
+        </Link>
+        {/* FanZone end */}
       </div>
     </section>
   )
