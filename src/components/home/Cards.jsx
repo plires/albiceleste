acfdb8ea48e717c987cf40dd8d0f931f09e49e47
Pablo Lires <pablo@librecomunicacion.net>
2025-06-10 16:28:07 -0300

    Cambios en cards

--- src/components/home/Cards.jsx
+++ src/components/home/Cards.jsx
@@ -11,29 +11,33 @@ const Cards = () => {
   return (
     <section className='cards container'>
       <div className='row'>
-        <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
-          <Link to='/miami'>
-            <img className='img-fluid' src={miami} alt='miami card' />
-          </Link>
-        </div>
-        <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
-          <Link to='/fan-zone'>
-            <img className='img-fluid' src={fanZone} alt='fan zone card' />
-          </Link>
-        </div>
-        <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
-          <Link to='/casa-campeon'>
-            <img
-              className='img-fluid'
-              src={casaCampeon}
-              alt='la casa del campeon card'
-            />
-          </Link>
-        </div>
-        <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
-          <Link to='/tienda'>
-            <img className='img-fluid' src={tienda} alt='tienda card' />
-          </Link>
+        <div className='col-lg-10 offset-lg-1'>
+          <div className='row'>
+            <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
+              <Link to='/miami'>
+                <img className='img-fluid' src={miami} alt='miami card' />
+              </Link>
+            </div>
+            <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
+              <Link to='/fan-zone'>
+                <img className='img-fluid' src={fanZone} alt='fan zone card' />
+              </Link>
+            </div>
+            <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
+              <Link to='/casa-campeon'>
+                <img
+                  className='img-fluid'
+                  src={casaCampeon}
+                  alt='la casa del campeon card'
+                />
+              </Link>
+            </div>
+            <div data-aos='fade-up' className='col-sm-6 p-0 contentCard'>
+              <Link to='/tienda'>
+                <img className='img-fluid' src={tienda} alt='tienda card' />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </section>
