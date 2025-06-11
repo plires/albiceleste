88b820e87093414d62fd7cc1dfd19391053c7fcf
Pablo Lires <pablo@librecomunicacion.net>
2025-06-11 00:51:42 -0300

    Creacion de page CulturaAlbiceleste

--- src/pages/CulturaAlbiceleste.jsx
+++ src/pages/CulturaAlbiceleste.jsx
@@ -1,13 +1,49 @@
+import slideMobile from '@/assets/img/cultura/header-cultura-mobile.webp'
+import slideDesktop from '@/assets/img/cultura/header-cultura.webp'
+import DetailsSection from '@/components/commons/DetailsSection.jsx'
+import Separador from '@/components/commons/Separador.jsx'
+
+import { getItemsCasa } from '@/utils/dataUtils.js'
+
+import './sub-sections.css'
+
 const CulturaAlbiceleste = () => {
+  const items = getItemsCasa('items')
   return (
-    <section className='culturaAlbiceleste container'>
-      <div className='row content'>
-        <div data-aos='fade-up' className='col-lg-12'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque amet
-          provident, quas vel nemo eaque tempore dolore sapiente repudiandae
-          pariatur possimus, rerum illo ratione sint error eius a sit laborum.
+    <section data-aos='fade-up' className='subSections culturaAlbiceleste'>
+      <div className='contentTitle'>
+        <h1>SHOWS, FIESTAS Y EVENTOS CULTURALES</h1>
+      </div>
+      <picture>
+        <source media='(max-width: 768px)' srcSet={slideMobile} />
+        <img
+          data-aos='fade-up'
+          className='img-fluid imgHeader'
+          src={slideDesktop}
+          alt='slide albiceleste'
+        />
+      </picture>
+
+      <div className='text container'>
+        <div className='row'>
+          <div className='col-lg-10 offset-lg-1'>
+            <p data-aos='fade-up'>
+              Como parte del universo Albiceleste, se desarrollará un calendario
+              importante de eventos con foco en la música, el arte y la cultura
+              argentina.
+            </p>
+            <p data-aos='fade-up'>
+              Desde fiestas oficiales durante el Mundial, recitales de los
+              artistas más populares o muestras de arte de referentes locales,
+              todo el contenido está diseñado para prolongar la experiencia de
+              los hinchas más allá del partido y consolidar nuevos espacios de
+              encuentro en torno a la pasión argentina.
+            </p>
+          </div>
         </div>
       </div>
+
+      <Separador size='sm' />
     </section>
   )
 }
