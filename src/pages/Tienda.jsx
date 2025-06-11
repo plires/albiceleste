0707f4c6db8457fc1ce2dd597ebd34c385272f99
Pablo Lires <pablo@librecomunicacion.net>
2025-06-11 09:56:25 -0300

    creacion temporal de page tienda

--- src/pages/Tienda.jsx
+++ src/pages/Tienda.jsx
@@ -1,13 +1,36 @@
+import slideMobile from '@/assets/img/slide-mobile.webp'
+import slideDesktop from '@/assets/img/slide-desktop.webp'
+
+import './tienda.css'
+
 const Tienda = () => {
   return (
-    <section className='tienda container'>
-      <div className='row content'>
-        <div data-aos='fade-up' className='col-lg-12'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque amet
-          provident, quas vel nemo eaque tempore dolore sapiente repudiandae
-          pariatur possimus, rerum illo ratione sint error eius a sit laborum.
+    <section className='tienda'>
+      <section data-aos='fade-up'>
+        <picture>
+          <source media='(max-width: 768px)' srcSet={slideMobile} />
+          <img
+            className='img-fluid'
+            src={slideDesktop}
+            alt='slide albiceleste'
+          />
+        </picture>
+      </section>
+
+      <section className='container info'>
+        <div className='row'>
+          <div className='col-lg-10 offset-lg-1'> </div>
         </div>
-      </div>
+        <div className='contentTitle'>
+          <h1 className='anton'>Tienda Albiceleste</h1>
+        </div>
+        <h2 className='anton'>La pasión no solo se grita, también se viste.</h2>
+        <p>
+          Muy pronto vas a encontrar productos Albiceleste para alentar desde
+          cualquier rincón del mundo. <br />
+          Próximamente...
+        </p>
+      </section>
     </section>
   )
 }
